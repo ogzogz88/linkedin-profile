614d75c0ecb083a436b28f938c80de7213edc663
Oguzhan Alagoz <ogzogz88@gmail.com>
2021-01-16 17:09:53 +0300

    Add jest and firebase hosting configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders welcome message', () => {
+  render(<App />);
+  expect(screen.getByText('Home')).toBeInTheDocument();
 });
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
